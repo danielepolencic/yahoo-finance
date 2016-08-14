2d6ec9f6e5765265e9a7f127a7fc238be6d661d7
Stéphane P. Péricat <contact@pericat.net>
2016-08-14 18:04:30 -0500

    ticker search

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ You can get news headlines for multiple tickers.
 api
   .getHeadlines('AAPL,YHOO,MSFT')
   .then(function(res) {
-    console.log("HEADLINES ?", res[0].item);
+    console.log("HEADLINES ?", res.item);
   })
 ```
 
@@ -32,7 +32,7 @@ api
 api
   .getQuote('AAPL')
   .then(function(res) {
-    console.log("QUOTE ?", res.query.results.quote);
+    console.log("QUOTE ?", res.quote);
   });
 ```
 
@@ -42,6 +42,16 @@ api
 api.
   getHistoricalData('AAPL', '2016-01-01', '2016-02-01')
   .then(function(res) {
-    console.log("DATA ?", res.query.results);
+    console.log("DATA ?", res.data);
+  });
+```
+
+## Ticker Search
+
+```js
+api.
+  ticker('Apple')
+  .then(function(res) {
+    console.log("TICKER ?", res.search, res.results);
   });
 ```
--- demo.js
+++ demo.js
@@ -5,17 +5,24 @@ var api = new YahooFinanceAPI();
 // api
 //   .getHeadlines('AAPL,YHOO,MSFT')
 //   .then(function(res) {
-//     console.log("HEADLINES ?", res[0].item);
+//     console.log("HEADLINES ?", res.item);
 //   })
-//
+
 // api
 //   .getQuote('AAPL')
 //   .then(function(res) {
-//     console.log("QUOTE ?", res.query.results.quote);
+//     console.log("QUOTE ?", res.quote);
 //   });
 
+// api.
+//   getHistoricalData('AAPL', '2016-01-01', '2016-02-01')
+//   .then(function(res) {
+//     console.log("DATA ?", res.data);
+//   });
+
+
 api.
-  getHistoricalData('AAPL', '2016-01-01', '2016-02-01')
+  ticker('Apple')
   .then(function(res) {
-    console.log("DATA ?", res.query.results);
+    console.log("TICKER ?", res.search, res.results);
   });
