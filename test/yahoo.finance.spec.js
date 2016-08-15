59b6c11c4729ed6e51b7b9670d5fc6f5ef8d4e09
Stéphane P. Péricat <contact@pericat.net>
2016-08-14 19:48:08 -0500

    test suite

--- test/yahoo.finance.spec.js
+++ test/yahoo.finance.spec.js
@@ -2,6 +2,8 @@ var expect = require('expect.js'),
     YahooFinanceAPI = require('../lib/yahoo.finance.js');
 
 describe('The Yahoo Finance module', function() {
+  this.timeout(5000);
+
   var api;
 
   beforeEach(function() {
@@ -12,6 +14,10 @@ describe('The Yahoo Finance module', function() {
     api = null;
   });
 
+  it('should have an "endpoints" object', function() {
+    expect(api.endpoints).to.be.an('object');
+  });
+
   it('should be able to find an array of tickers for a given search term', function() {
     return api
       .ticker('Apple')
@@ -19,6 +25,32 @@ describe('The Yahoo Finance module', function() {
         expect(res.search).to.equal('Apple');
         expect(res.results).to.be.an('array');
         expect(res.results[0]).to.be.an('object');
+      });
+  });
+
+  it('should be able to get the latest quote for a given ticker', function() {
+    return api
+      .getQuote('AAPL')
+      .then(function(res) {
+        expect(res.quote).to.be.an('object');
+        expect(res.quote.symbol).to.equal('AAPL');
+      });
+  });
+
+  it('should be able to get some headlines for a given ticker', function() {
+    return api
+      .getHeadlines('AAPL')
+      .then(function(res) {
+        expect(res.headlines).to.be.an('array');
+      });
+  });
+
+  it('should be able to get some historical data for a given ticker', function() {
+    return api
+      .getHistoricalData('AAPL', '2016-01-01', '2016-02-01')
+      .then(function(res) {
+        expect(res.data).to.be.an('array');
+        expect(res.data.length).to.equal(20);
       });
   });
 });
