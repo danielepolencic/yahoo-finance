4e26379b4b53d8df1f122a0661f569efbedca450
Stéphane P. Péricat <contact@pericat.net>
2016-08-14 20:54:21 -0500

    code cleanup

--- demo.js
+++ demo.js
@@ -2,24 +2,28 @@ var YahooFinanceAPI = require('./build/yahoo.finance.js');
 
 var api = new YahooFinanceAPI();
 
+// HEADLINES
 api
   .getHeadlines('AAPL,YHOO,MSFT')
   .then(function(res) {
     console.log("HEADLINES ?", res.headlines);
-  })
+  });
 
+// QUOTE
 api
   .getQuote('AAPL')
   .then(function(res) {
     console.log("QUOTE ?", res.quote);
   });
 
+// HISTORICAL DATA
 api.
   getHistoricalData('AAPL', '2016-01-01', '2016-02-01')
   .then(function(res) {
     console.log("DATA ?", res.data);
   });
 
+// TICKER
 api.
   ticker('Apple')
   .then(function(res) {
