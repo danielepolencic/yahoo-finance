315d6fbf64eec7a325fb401c6093cbc3fb878e17
Stéphane P. Péricat <contact@pericat.net>
2017-08-06 16:57:15 -0500

    update demo

--- demo.js
+++ demo.js
@@ -33,43 +33,6 @@ router.get('/quote/realtime/:tickers', (req, res) => {
 });
 
 /**
- * @desc historical data
- * @example http://localhost:3000/api/quote/historical/yhoo/2017-01-01/2017-02-01
- */
-router.get('/quote/historical/:ticker/:start/:end', (req, res) => {
-  let {ticker, start, end} = req.params;
-
-  api
-    .getHistoricalData(ticker, start, end)
-    .then(data => res.json(data))
-    .catch(err => res.json(err));
-});
-
-/**
- * @desc Dividends history
- * @example http://localhost:3000/api/dividends/history/aapl/2016-01-01/2016-12-31
- */
-router.get('/dividends/history/:ticker/:start/:end', (req, res) => {
-  let {ticker, start, end} = req.params;
-
-  api
-    .getDividendsHistory(ticker, start, end)
-    .then(data => res.json(data))
-    .catch(err => res.json(err));
-});
-
-/**
- * @desc List of securities by sector id
- * @example http://localhost:3000/api/securities/bysector/812
- */
-router.get('/securities/bysector/:sectorid', (req, res) => {
-  api
-    .getSecuritiesBySectorIndex(req.params.sectorid)
-    .then(data => res.json(data))
-    .catch(err => res.json(err));
-});
-
-/**
  * @desc Forex data
  * @example http://localhost:3000/api/forex/eurusd,gbpusd,cadusd
  */
@@ -87,17 +50,6 @@ router.get('/forex/:exchanges', (req, res) => {
 router.get('/news/headlines/:ticker', (req, res) => {
   api
     .getHeadlinesByTicker(req.params.ticker)
-    .then(data => res.json(data))
-    .catch(err => res.json(err));
-});
-
-/**
- * @desc Chart data
- * @example http://localhost:3000/api/chart/intraday/aapl
- */
-router.get('/chart/intraday/:ticker', (req, res) => {
-  api
-    .getIntradayChartData(req.params.ticker)
     .then(data => res.json(data))
     .catch(err => res.json(err));
 });
