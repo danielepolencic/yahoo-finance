8a4de0e8bac83468d2857a6138b86ffc4c7d912d
Stéphane P. Péricat <contact@pericat.net>
2017-08-26 16:32:22 -0500

    futures and commodities

--- demo.js
+++ demo.js
@@ -125,11 +125,40 @@ router.get('/ticker/recommendations/:ticker', (req, res) => {
    .catch(err => res.json(err));
 });
 
+/**
+ * @desc futures
+ * @example http://localhost:3000/api/markets/futures?market=NQ=F
+ *
+ * S&P 500: ES=F
+ * NASDAQ: NQ=F
+ * DOW JONES: YM=F
+ */
+router.get('/markets/futures', (req, res) => {
+  api
+    .futures(req.query.market)
+    .then(data => res.json(data))
+    .catch(err => res.json(err));
+});
+
+/**
+ * @desc futures
+ * @example http://localhost:3000/api/markets/commodities?commodities=GC=F,SI=F,PL=F,HG=F
+ */
+router.get('/markets/commodities', (req, res) => {
+  api
+    .commodities(req.query.commodities)
+    .then(data => res.json(data))
+    .catch(err => res.json(err));
+});
+
 app.use('/api', router);
 
+// Demo HTML page at : http://localhost:3000/
 app.get('/', (req, res) => res.sendFile(__dirname + '/demo.html'));
 
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000/api');
+  console.log('Server started on http://localhost:3000/');
+  console.log('API available at http://localhost:3000/api');
+
   open('http://localhost:3000/');
 });
